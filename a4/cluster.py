ff5a88d59934225cab4937d9d5fdc8289f388756
raj-kotak <raj.kotak1192@gmail.com>
2017-11-15 15:03:52 -0600

    collect and cluster files almost done

--- a4/cluster.py
+++ a4/cluster.py
@@ -1,3 +1,61 @@
 """
 cluster.py
 """
+from collections import Counter, defaultdict
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import time
+
+
+def readFromFile(filename):
+    users_friends_dict = defaultdict(lambda:0)
+    f = open(filename, 'r+')
+    data = f.readlines()
+    for d in data:
+        split_list = d.split(':')
+        split_list[1] = split_list[1].strip(" ")
+        split_list[1] = split_list[1].replace("[","")
+        split_list[1] = split_list[1].replace("]","")
+        split_list[1] = split_list[1].replace(" ","")
+        split_list[1] = split_list[1].split(",")
+        users_friends_dict[split_list[0]] = split_list[1]
+    return users_friends_dict
+
+def createGraph(users_friends_dict):
+    graph = nx.Graph()
+    for user, friends in users_friends_dict.items():
+        graph.add_node(user)
+        for friend in friends:
+            graph.add_edge(friend, user)
+    
+    removenodes = []
+    for node, degree in graph.degree().items():
+       if degree <= 1:
+          removenodes.append(node)          
+    graph.remove_nodes_from(removenodes)
+
+    return graph
+
+def findMaxEdge(graph):
+    betweenness = nx.edge_betweenness_centrality(graph)
+    print(max(betweenness.keys()))
+    return max(betweenness.keys())
+
+def createComponents(graph):
+    components = [c for c in nx.connected_component_subgraphs(graph)]
+
+    while len(components) == 1:
+        edge_to_remove = findMaxEdge(graph)
+        graph.remove_edge(*edge_to_remove)
+        components = [c for c in nx.connected_component_subgraphs(graph)]
+    return components
+
+def main():
+    users_friends_dict = readFromFile('users_friends.txt')
+    graph = createGraph(users_friends_dict)
+    components = createComponents(graph)
+    # print(len(graph.nodes()), len(graph.edges()))
+
+if __name__ == '__main__':
+    main()
--- a4/collect.py
+++ a4/collect.py
@@ -1,3 +1,62 @@
 """
 collect.py
 """
+from collections import Counter
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import time
+from TwitterAPI import TwitterAPI
+
+consumer_key = 'VA00WIXDSMZMiDFWmr8aylbDr'
+consumer_secret = 'gFckFgd3kxzK284j28DQvniUoLeO5u8hVZRD1OtFGAgErmiXFu'
+access_token = '3738267792-FEw1oJ1dqQQJG74MWeRv2fPf7Z6czrikpvUV81H'
+access_token_secret = '595OxWzmCClyKvxvZKOAgFGhwdn85IkKNBeQZSP0SCnxH'
+
+def get_twitter():
+    return TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+
+def robust_request(twitter, resource, params, max_tries=5):
+    for i in range(max_tries):
+        request = twitter.request(resource, params)
+        if request.status_code == 200:
+            return request
+        else:
+            print('Got error %s \nsleeping for 15 minutes.' % request.text)
+            sys.stderr.flush()
+            time.sleep(61 * 15)
+
+def get_tweets(twitter):
+  tweets = robust_request(twitter, 'search/tweets', {'count':100,'q':'#cryptocurrency -filter:retweets'}, max_tries=5)
+  print("no of tweets are ", len([print(t['text'], "\n") for t in tweets]))
+  f = open('tweets_demo.txt', 'w+', encoding='utf-8')
+  for tweet in tweets:
+    f.write(tweet['text']+"\n")
+  f.close()
+
+  return tweets
+
+def get_users_and_friends(twitter, tweets):
+  f = open('users_friends_demo.txt', 'w+')
+
+  users_list = []
+  for tweet in tweets:
+    users_list.append(tweet['user']['screen_name'])
+  print("no of users are ", len(set(users_list)))
+  user_friends_list = []
+  for user in set(users_list):
+    user_friends_req = robust_request(twitter, 'friends/ids', {'screen_name':user, 'count':10}, max_tries=5)
+    for friends in user_friends_req:
+      user_friends_list.append(friends)
+    f.write(user+":"+str(sorted(user_friends_list))+"\n")
+    user_friends_list = []
+  
+  f.close()
+
+def main():
+  twitter = get_twitter()
+  tweets = get_tweets(twitter)
+  get_users_and_friends(twitter, tweets)
+
+if __name__ == '__main__':
+    main()
